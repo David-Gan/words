15b56c7384bf3bd0acdc170302322ecdc519e355
mu.gan <mu.gan@nio.com>
2019-12-19 14:31:00 +0800

    add source url

--- views/show.php
+++ views/show.php
@@ -5,12 +5,21 @@ $lines = explode("\n", $data);
 foreach ($lines as $line) {
 	$line = trim($line);
 	if (empty($line)) continue;
-	$content .= '<p><a href="' . $url . urlencode($line) . '">' . $line . '</a></p>';
+
+	if (substr($line, 0, 1) === '-') {
+		$line = substr($line, 1);
+		$content .= '<p><a class="source" target="_blank" href="' . $line . '">' . $line . '</a></p>';
+	} else {
+		$content .= '<p><a href="' . $url . urlencode($line) . '">' . $line . '</a></p>';	
+	}
 }
 ?>
 <style type="text/css">
 	.container a {
 		color: black;
+	}
+	.container .source {
+		color: #aaa;
 	}
 </style>
 <div class="container" style="margin-top:20px;">
